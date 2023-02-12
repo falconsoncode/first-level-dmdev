f789d3dba3fe65b23a5f20e7a8aff0ed73dc44df
vsokolov <v.sokolov@consaltica.ru>
2023-02-12 15:21:06 +0400

    Homework 2: task 3

---
+++ src/com/dmdev/cs/homework/homework_2/Task3.java
@@ -0,0 +1,33 @@
+package com.dmdev.cs.homework.homework_2;
+
+/**
+ * Программист Ваня сразу после окончания курсов dmdev устроился в IT компанию на позицию Junior Java Developer с зарплатой 600$ в месяц.
+ * Ему обещали, что будут поднимать зарплату на 400$ каждые 6 месяцев.
+ * 300$ в месяц Ваня тратит на еду и развлечения.
+ * 10% от зарплаты Ваня ежемесячно переводит на счет брокера, чтобы инвестировать в акции с доходностью 2% в месяц.
+ * <p>
+ * Посчитать, сколько Ваня будет иметь средств на своем счету и на счету брокера за 3 года и 2 месяца.
+ * <p>
+ * Для интереса: попробовать увеличить процент, которые Ваня инвестирует из своей зарплаты
+ */
+public class Task3 {
+    public static void main(String[] args) {
+        double salary = 600.0;
+        double expenses = 300.0;
+        double investment = 0.0;
+        double brokerAccount = 0.0;
+        double personalAccount = 0.0;
+        int time = 38;
+        for (int i = 1; i < time; i++) {
+            if (i > 0 && i % 6 == 0) {
+                salary += 400.0;
+            }
+            investment = salary * 0.1;
+            brokerAccount += investment * 0.02;
+            personalAccount = salary - expenses - investment;
+        }
+        System.out.println("Salary: " + salary);
+        System.out.println("Broker: " + brokerAccount);
+        System.out.println("Account: " + personalAccount);
+    }
+}
